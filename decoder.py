f8eca2932e10898d8e066c250e8e49c90d433108
Robert69nice <robert.adelt@gmail.com>
2024-01-16 22:16:55 +0100

    try except added to decoder and file_test.py deleted

--- decoder.py
+++ decoder.py
@@ -2,15 +2,27 @@ import ast
 import os
 
 key = []
+start = []
 
 def filekey(keylist):
     temp = ast.literal_eval(keylist)
+    start.append(temp[-1][0])
     for i in range(len(temp)):
         key.append(temp[i])
 
 def decrypter(message):
-    decrypted = []
-    for i in range(len(key)): 
-        decrypted.append(key[i][message[i]])
-    text = ''.join(decrypted)
+    try:
+        stop = int(start[0])+len(message)
+        decrypted = []
+        temp = []
+        for i in range(start[0], min(stop, len(key))): 
+            temp.append(key[i])
+
+        for i in range(len(temp)):
+            decrypted.append(temp[i][message[i]])
+        text = ''.join(decrypted)
+    except IndexError:
+        text = 'key and message do not match'
+    except TypeError:
+        text = 'key and message do not match'
     return text
